539cb038648939d2bbe20470e9696776d7bd80bb
Ryan Hostager <hostager.ryan@gmail.com>
2019-10-09 22:05:19 -0500

    update

--- password.js
+++ password.js
@@ -1,12 +1,10 @@
 generatePasswords = function() {
 	document.body.style.backgroundColor = "lightblue";
-	tt = document.createElement("table");
-	tt.setAttribute('id', 'pwTable');
-	tt.setAttribute('border', '1');
+	//tt = document.createElement("table");
 	t = document.querySelector("#pwTable");
 	trow = document.createElement("tr");
     td = document.createElement("td");
-	contents = document.createTextNode(Date());
+	contents = document.createTextNode("HI");
 	td.appendChild(contents);
 	td.onclick = clickon;
 	trow.appendChild(td);
