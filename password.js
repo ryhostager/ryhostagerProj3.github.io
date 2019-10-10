6afe3d1184a9a13a85aff4db3bec23f91825cc0d
Ryan Hostager <hostager.ryan@gmail.com>
2019-10-10 02:08:55 -0500

    update

--- password.js
+++ password.js
@@ -10,6 +10,7 @@ function generatePasswords() {
 	max = document.querySelector("#maxWordLength").value;
 	pwLength = document.querySelector("#maxPWLength").value;
 	numSubs = document.querySelector("#subNums").checked;
+	document.body.deleteChild("ul")
     ul = document.createElement("ul");
 	
 	for(i = 0; i < 20; i++) {
