35ed5802f061a7dda1cd8b2209e4850de12ec9c5
Ryan Hostager <hostager.ryan@gmail.com>
2019-10-09 21:54:48 -0500

    table stuff

--- password.js
+++ password.js
@@ -1,3 +1,14 @@
 generatePasswords = function() {
 	document.body.style.backgroundColor = "lightblue";
+	tt = document.createElement("table");
+	tt.setAttribute('id', 'pwTable');
+	tt.setAttribute('border', '1');
+	t = document.querySelector("#pwTable");
+	trow = document.createElement("tr");
+    td = document.createElement("td");
+	contents = document.createTextNode(Date());
+	td.appendChild(contents);
+	td.onclick = clickon;
+	trow.appendChild(td);
+	t.appendChild(trow);
 }
